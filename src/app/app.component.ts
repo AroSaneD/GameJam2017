435b54c3b52255ac9b59a1924bba76ca93a2ab1c
AroSaneD <mail.junk.3135@gmail.com>
2017-04-22 02:24:21 +0300

    Added ability to responde via swipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Card } from './model/Card';
 import { DecisionTree } from './model/DecisionTree';
 import { Http } from '@angular/http';
@@ -13,9 +13,27 @@ import { CardService } from './services/CardService';
   providers: [CardService]
 })
 export class AppComponent {
+  private readonly maxXOffset: number = 130;
+  private readonly maxRotation: number = 5;
+  private readonly maxYOffset: number = 40;
+
   title = 'app works12313!';
   currentCard: Card;
   decisions: DecisionTree;
+
+  cardXCoordinate: number = 0;
+
+  private get cardRotation(): number {
+    return Math.abs(this.cardXCoordinate) / this.maxXOffset * this.maxRotation * Math.sign(this.cardXCoordinate);
+  };
+
+  private get cardYCoordinate(): number {
+    return Math.abs(this.cardXCoordinate) / this.maxXOffset * this.maxYOffset;
+  };
+
+  isDragging: boolean;
+
+  //@ViewChild('playingCard') cardRef: ElementRef;
 
   constructor(private cardService: CardService) {
 
@@ -33,10 +51,46 @@ export class AppComponent {
   }
 
   private clickLeft() {
-    alert("Click left");
+    //alert("Click left");
   }
 
   private clickRight() {
-    alert("Click right");
+    //alert("Click right");
   }
+
+  dragImageStart() {
+    this.isDragging = true;
+  }
+
+  dragImageEnd() {
+    this.isDragging = false;
+    if (this.cardXCoordinate >= 0) {
+      this.clickRight();
+    } else {
+      this.clickLeft();
+    }
+
+    //this.cardXCoordinate = 0;
+    let intervalId = setInterval(() => {
+      this.cardXCoordinate -= this.cardXCoordinate / 20;
+      if (Math.abs(this.cardXCoordinate) < 1) {
+        this.cardXCoordinate = 0;
+        clearInterval(intervalId);
+      }
+    }, 5);
+  }
+
+  dragImage(event: MouseEvent) {
+    if (!this.isDragging) {
+      return;
+    }
+
+    if (Math.abs(this.cardXCoordinate + event.movementX) > this.maxXOffset) {
+      this.cardXCoordinate += (this.maxXOffset - Math.abs(this.cardXCoordinate)) * Math.sign(this.cardXCoordinate);
+      return;
+    }
+
+    this.cardXCoordinate += event.movementX;
+  }
+
 }
